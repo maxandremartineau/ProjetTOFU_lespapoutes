686f15c425fa241a33c582a548b1adf5dc3c26aa
maxandremartineau <2463096@csfoy.ca>
2025-11-26 09:14:18 -0500

    Ajout du lien pour afficher et autres.

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ $niveau="./";
     <meta name="keyword" content="">
     <meta name="author" content="">
     <meta charset="utf-8">
-    <title>Un beau titre ici!</title>
+    <title>Accueil Projet TOFU</title>
     <?php require_once($niveau.'liaisons/inc/fragments/head_links.inc.php');?>
 </head>
 
@@ -26,6 +26,30 @@ $niveau="./";
 
 <?php
 //------------------------------------------------------------
+// Récupération de l'id de la liste dans l'URL
+//------------------------------------------------------------
+
+if (isset($_GET['id_liste'])) {
+
+    // Sécuriser la valeur
+    $idListe = intval($_GET['id_liste']);
+
+    $strRequete = "
+        SELECT *
+        FROM items
+        WHERE liste_id = $idListe
+    ";
+
+    $pdos = $pdoConnexion->query($strRequete);
+
+} else {
+
+    // Message propre si aucun ID n'est fourni
+    $messageErreur = "Aucune liste sélectionnée.";
+
+}
+
+//------------------------------------------------------------
 // CHARGER LA LISTE DES LISTES AVEC LE NOMBRE D’ITEMS
 //------------------------------------------------------------
 
@@ -35,18 +59,17 @@ $strRequeteListe = "
         listes.nom,
         listes.couleur_id,
         couleurs.hexadecimal AS couleur_hex,
-        COUNT(items.id) AS nb_items
+        (
+            SELECT COUNT(*) 
+            FROM items 
+            WHERE items.liste_id = listes.id
+        ) AS nb_items
     FROM listes
-    JOIN couleurs 
+    INNER JOIN couleurs 
         ON listes.couleur_id = couleurs.id
-    LEFT JOIN items
-        ON items.liste_id = listes.id
-    GROUP BY 
-        listes.id,
-        listes.nom,
-        listes.couleur_id,
-        couleurs.hexadecimal
 ";
+
+
 
 $pdosResultat = $pdoConnexion->query($strRequeteListe);
 
@@ -69,7 +92,7 @@ while ($ligne = $pdosResultat->fetch()) {
 $pdosResultat->closeCursor();
 ?>
 
-<h1 class="text-white font-bold text-5xl">Listes :</h1>
+<h1 class="text-white font-bold text-5xl">Vos listes :</h1>
 
 <!-- ------------------------------------------------------------
 FORMULAIRE #1 : LISTES
@@ -88,21 +111,23 @@ for ($intCptListes = 0; $intCptListes < count($arrListes); $intCptListes++) {
     $nbItems = $arrListes[$intCptListes]["nb_items"];
 
     echo "
-    <li class='bg-[#463f6b] border-3 border-white/20 py-4 px-5 flex items-center justify-between m-2'>
+    <li class='bg-[#463f6b] border-3 border-white/20 py-8 px-6 flex items-center justify-between m-8 max-w-5xl mx-auto relative'>
 
         <div class='flex items-center gap-3'>
             <span class='w-3 h-3 rounded-full' style='background-color: #$couleurHex;'></span>
-            <span class='font-semibold'>$nomListe ($nbItems)</span>
+            <h2 class='font-semibold '>$nomListe ($nbItems)</h2>
+            <a href='" . $niveau . "items/afficher.php?id_liste=$idListe' class='absolute inset-0 z-0'></a>
         </div>
 
-        <div class=\"flex items-center gap-6\">
-            <a href='../maj/index.php?id_liste=$idListe' class='flex items-center gap-1 hover:underline'>
-                <img src='liaisons/images/icons/edit.svg' class='w-5' alt=''> Modifier
+        <div class='flex items-center gap-6 relative'>
+            <a href='../maj/index.php?id_liste=$idListe' class='flex items-center gap-1 hover:underline hover:text-[#FF66D6]'>
+                <img src='liaisons/images/icons/edit.svg' class='w-6 hover:text-[#FF66D6]' alt=''> Modifier
             </a>
-
-            <a href='#' class='flex items-center gap-1 hover:underline'>
-                <img src='liaisons/images/icons/remove.svg' class='w-5' alt=''> Supprimer
-            </a>
+            <div class='hover:underline hover:text-[#FF66D6] relative'>
+                <a href='#' class='flex items-center gap-1 '>
+                    <img src='liaisons/images/icons/remove.svg' class='w-5' alt=''> Supprimer
+                </a>
+            </div>
         </div>
 
     </li>";
@@ -113,12 +138,6 @@ for ($intCptListes = 0; $intCptListes < count($arrListes); $intCptListes++) {
 </form>
 
 </main>
-
-<aside class="text-white">
-    <h3>Barre latérale</h3>
-</aside>
-
-</div>
 
 <footer>
     <?php include ($niveau . "liaisons/inc/fragments/pied_de_page.inc.php");?>
